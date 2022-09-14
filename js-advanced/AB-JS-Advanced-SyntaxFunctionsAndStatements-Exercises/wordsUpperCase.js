e24060d7b465b7278ab8ae843ad11f46562f58bd
yetoshimo <yetmenyilmaz@gmail.com>
2022-09-14 18:08:49 +0300

    edited exercise

--- js-advanced/AB-JS-Advanced-SyntaxFunctionsAndStatements-Exercises/wordsUpperCase.js
+++ js-advanced/AB-JS-Advanced-SyntaxFunctionsAndStatements-Exercises/wordsUpperCase.js
@@ -1,11 +1,11 @@
 function wordsUpperCase(givenText) {
     let wordRegex = /(\w+)/g;
-    let matches = [];
-    givenText.match(wordRegex).forEach(element => {
-        matches.push(element.toUpperCase());
-    });
+    // let matches = [];
+    // givenText.match(wordRegex).forEach(element => {
+    //     matches.push(element.toUpperCase());
+    // });
 
-    console.log(matches.join(', '))
+    console.log(givenText.match(wordRegex).join(', ').toUpperCase())
 }
 
 wordsUpperCase('Hi, how are you?')
